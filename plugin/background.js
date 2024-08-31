bb915a22165d9a097c4f991fe79a343b93bf2d6f
ytakahashi <yt01tc@gmail.com>
2024-08-31 20:16:10 +0900

    feat: update click event (action)

--- plugin/background.js
+++ plugin/background.js
@@ -1,3 +1,31 @@
+const copyAndNotify = value => {
+  const popup = () => {
+    const popupWindow = document.createElement('div')
+    popupWindow.classList.add('popup-window')
+    const popupText = document.createElement('div')
+    popupText.classList.add('popup-text')
+
+    popupText.innerHTML = `<p>Copied!: ${value}</p>`
+
+    popupWindow.appendChild(popupText)
+    document.body.appendChild(popupWindow)
+
+    const modalMask = document.createElement('div')
+    modalMask.classList.add('modal-mask')
+    document.body.appendChild(modalMask)
+
+    const removePopup = () => {
+      document.body.removeChild(popupWindow)
+      document.body.removeChild(modalMask)
+    }
+
+    document.body.addEventListener('click', removePopup)
+    setTimeout(removePopup, 4000)
+  }
+
+  navigator.clipboard.writeText(value).then(popup)
+}
+
 chrome.runtime.onInstalled.addListener(() => {
   const parent = chrome.contextMenus.create({
     id: 'parent',
@@ -61,16 +89,23 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 })
 
-chrome.action.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener(async tab => {
+  const result = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: createMarkdown,
+  })
+  const value = result[0].result
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: mdWithConfirm,
+    function: copyAndNotify,
+    args: [value],
   })
 })
 
 const url = () => navigator.clipboard.writeText(location.href)
 const title = () => navigator.clipboard.writeText(document.title)
 const md = () => navigator.clipboard.writeText(`[${document.title}](${location.href})`)
+const createMarkdown = () => `[${document.title}](${location.href})`
 const html = () => {
   const body = `<a href="${location.href}">${document.title}</a>`
   const blob = new Blob([body], { type: 'text/html' })
@@ -78,10 +113,4 @@ const html = () => {
   const item = [new window.ClipboardItem({ 'text/html': blob, 'text/plain': blobPlain })]
 
   navigator.clipboard.write(item)
-}
-
-const mdWithConfirm = () => {
-  const mdStr = `[${document.title}](${location.href})`
-  navigator.clipboard.writeText(mdStr)
-  confirm(mdStr)
 }
